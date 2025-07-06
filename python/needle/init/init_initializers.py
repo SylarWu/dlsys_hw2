2aa52c5494dc364f4b9d2c5b0fd524b9831e9041
sylarw <xlwuuu@163.com>
2025-07-06 16:24:19 +0800

    homework 2: Q0(100%), Q1(100%)

--- python/needle/init/init_initializers.py
+++ python/needle/init/init_initializers.py
@@ -4,25 +4,45 @@ from .init_basic import *
 
 def xavier_uniform(fan_in, fan_out, gain=1.0, **kwargs):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    bound = gain * math.sqrt(6.0 / (fan_in + fan_out))
+    return rand(fan_in, fan_out, low=-bound, high=bound, **kwargs)
     ### END YOUR SOLUTION
 
 
 def xavier_normal(fan_in, fan_out, gain=1.0, **kwargs):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    return randn(
+        fan_in, fan_out,
+        mean=0.0,
+        std=gain * math.sqrt(2.0 / (fan_in + fan_out)),
+        **kwargs
+    )
     ### END YOUR SOLUTION
+
+
+def gain_factor(nonlinearity="relu"):
+    default = math.sqrt(2.0)
+    mapping = {
+        "relu": default,
+    }
+    return default if nonlinearity not in mapping else mapping[nonlinearity]
+
 
 def kaiming_uniform(fan_in, fan_out, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    bound = gain_factor(nonlinearity) * math.sqrt(3.0 / fan_in)
+    return rand(fan_in, fan_out, low=-bound, high=bound, **kwargs)
     ### END YOUR SOLUTION
-
 
 
 def kaiming_normal(fan_in, fan_out, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    return randn(
+        fan_in, fan_out,
+        mean=0.0,
+        std=gain_factor(nonlinearity) / math.sqrt(fan_in),
+        **kwargs
+    )
     ### END YOUR SOLUTION
